1d2e051ec963ecbe844fd28b6d177cb9e918acbb
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2025-02-04 22:49:51 +0530

    feat(excom-page): create excome page

---
+++ src/app/excom/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Excom() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider text-center mt-10 mb-5">
+        EXECUTIVE COMMITTEE
+      </h1>
+      <p className=" w-auto lg:w-[1200px] my-6 text-center">
+        The Executive Committee of the IEEE NSBM Student Branch consists of the
+        Chair, the Vice Chair, the Secretary, Founders, and fellow
+        representatives from the Board. The purpose of the Executive Committee
+        is to manage the daily operations of the community. They are authorized
+        to establish and upgrade the rules, procedures, restrictions,
+        resolutions, and requirements of the Board, as well as the IEEE NSBM
+        Student Branch.
+      </p>
+    </div>
+  );
+}
