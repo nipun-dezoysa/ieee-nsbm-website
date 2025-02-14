f3a3f704f19ae250887d41b3d8a34cb46d4964dd
nipun-dezoysa <nipunavishka123@gmail.com>
2025-02-14 20:38:56 +0530

    feat: update event caeds with content

--- src/components/index/event-card.tsx
+++ src/components/index/event-card.tsx
@@ -6,66 +6,10 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { events } from "@/content/event-data";
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
-
-interface Event {
-  id: number;
-  title: string;
-  date: string;
-  description: string;
-  imageUrl: string;
-  category: string;
-}
-
-const events: Event[] = [
-  {
-    id: 1,
-    title: "අයුබෝවන් IEEE 2022",
-    date: "30th July, 2022",
-    description:
-      "The 'AYUBOWAN IEEE 2022' is a session that raises awareness among the university students on Introduction to IEEE Student Branch of NSBM, Advantages of IEEE Membership, and How to Become an IEEE Recognized individual.",
-    imageUrl: "https://placehold.co/300x150.png",
-    category: "sb",
-  },
-  {
-    id: 2,
-    title: "MAD FEST",
-    date: "25th May, 2022",
-    description:
-      "MAD-Fest is a platform which opens for passionate Mobile Application Developers.",
-    imageUrl: "https://placehold.co/300x150.png",
-    category: "cs",
-  },
-  {
-    id: 3,
-    title: "INTERNATIONAL ARDUINO DAY CELEBRATION 2022 AT NSBM",
-    date: "23rd March, 2022",
-    description:
-      "Arduino Day is a celebration of the Arduino Community and its achievements.",
-    imageUrl: "https://placehold.co/300x150.png",
-    category: "wie",
-  },
-  {
-    id: 4,
-    title: "DUOTHAN 2.0",
-    date: "23rd February, 2022",
-    description:
-      "Duothan 2.0 is a hackathon that is organized by the IEEE Student Branch of NSBM which is competed by groups of two. This hackathon is organized with the explicit purpose of enhancing the coding skills of undergraduates.",
-    imageUrl: "https://placehold.co/300x150.png",
-    category: "cs",
-  },
-  {
-    id: 5,
-    title: "ROAD TO FULL STACK - MOBILE",
-    date: "8th December, 2021",
-    description:
-      "Road To Full Stack - Mobile organized by IEEE Student Branch Affinity Group of NSBM is a great opportunity for you to begin the strategy of Mobile Application Development.",
-    imageUrl: "https://placehold.co/300x150.png",
-    category: "cs",
-  },
-];
 
 const eventcard = ({ category }: { category?: string }) => {
   const filteredEvents = category
@@ -77,17 +21,17 @@ const eventcard = ({ category }: { category?: string }) => {
 
       <Carousel className="w-full">
         <CarouselContent>
-          {filteredEvents.map((event) => (
+          {filteredEvents.map((event, index) => (
             <CarouselItem
-              key={event.id}
+              key={index}
               className="md:basis-1/2 sm:basis-1/2 lg:basis-1/3"
             >
               <Card className="h-[30rem] shadow-lg rounded-lg overflow-hidden">
-                <CardContent className="flex flex-col p-0 gap-5 h-full">
+                <CardContent className="flex flex-col p-0 gap-5 h-full relative">
                   <div className="w-full h-full space-y-4">
                     <div className="aspect-video relative w-full">
                       <Image
-                        src={event.imageUrl}
+                        src={event.image}
                         alt={event.title}
                         layout="fill"
                       />
@@ -96,14 +40,16 @@ const eventcard = ({ category }: { category?: string }) => {
                     <div className="px-4 flex flex-col gap-1">
                       <h2 className="text-xl font-semibold ">{event.title}</h2>
                       <p className="text-sm text-gray-500 text-left">
-                        {event.date}
+                        {event.year}
                       </p>
 
-                      <p className="text-sm">{event.description}</p>
+                      <p className="text-sm text-left line-clamp-6 overflow-hidden">
+                        {event.description}
+                      </p>
                     </div>
                   </div>
 
-                  <Button className="font-bold min-w-[130px] bg-primaryLight text-white rounded hover:bg-blue-700 transition-colors m-4">
+                  <Button className="font-bold min-w-[130px] bg-primaryLight text-white rounded hover:bg-blue-700 transition-colors mx-4 mb-4">
                     Read More
                   </Button>
                 </CardContent>
